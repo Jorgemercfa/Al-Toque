967e7380db3c8a95c96e5430a224c8bc5b8f2aea
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2026-03-11 11:48:22 -0500

    add_routers_views

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Home from './views/Home-item.vue';
 import About from './views/About-item.vue';
 import Coupons from './views/Coupon-item.vue';
 import Contact from './views/Contact-item.vue';
+import SignIn from './views/Sign-in.vue';
+import SignUp from './views/Sign-up.vue';
 import CouponsDetails from './components/Component-coupons-item.vue';
 
 const routes = [
@@ -11,6 +13,8 @@ const routes = [
   { path: '/About-item', name: 'About', component: About },
   { path: '/Coupon-item', name: 'Coupon', component: Coupons },
   { path: '/Contact-item', name: 'Contact', component: Contact },
+  { path: '/Sign-in', name: 'SignIn', component: SignIn },
+  { path: '/Sign-up', name: 'SignUp', component: SignUp },
   { path: '/coupon/:id', name: 'CouponsDetails', component: CouponsDetails }, // ✅ Ruta correcta
   { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
